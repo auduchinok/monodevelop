auduchinok/monodevelop

commit 533de660b6f57bfc5ba60bac0a94309d0183377f

[Core] Fix project options cannot be opened for generic projects
On trying to open the project options dialog for a generic project
a null reference exception was thrown by the RunConfigurationPanel
since it could not find an run configuration editor for generic
projects. To avoid this the generic project now indicates it does
not support run configurations so the project options dialog can
be opened.

main/src/core/MonoDevelop.Core/MonoDevelop.Projects/GenericProject.cs
@@ -67,6 +67,11 @@ namespace MonoDevelop.Projects
 			base.OnWriteConfiguration (monitor, config, pset);
 			pset.SetValue ("OutputPath", config.OutputDirectory);
 		}
+
+		protected override ProjectFeatures OnGetSupportedFeatures ()
+		{
+			return ProjectFeatures.Build | ProjectFeatures.Configurations | ProjectFeatures.Execute;
+		}
 	}
 	
 	[ProjectModelDataItem]

commit a634b040db66d0ac3f407de6a87725d073feadf5

[Project] Fix text failure introduced in 533de660b6f57

main/src/core/MonoDevelop.Core/MonoDevelop.Projects/GenericProject.cs
@@ -71,7 +71,8 @@ namespace MonoDevelop.Projects
 
 		protected override ProjectFeatures OnGetSupportedFeatures ()
 		{
-			return ProjectFeatures.Build | ProjectFeatures.Configurations | ProjectFeatures.Execute;
+			//there is no run config editor for generic projects
+			return base.OnGetSupportedFeatures () & ~ProjectFeatures.RunConfigurations;
 		}
 	}
 	